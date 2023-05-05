8da61e551999acb32523b9b8bbd21c0e5debef0c
Mike Johnson <mijohns@microsoft.com>
2023-05-05 21:07:05 +0000

    Added version and oidc

--- iac/providers.tf
+++ iac/providers.tf
@@ -3,10 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.47"
+      version = ">= 3.7.0"
     }
   }
 }
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
