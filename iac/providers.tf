5169ae9cb5d310857891fc6779e69f1e568e1905
Mike Johnson <mijohns@microsoft.com>
2023-05-17 12:44:15 -0600

    Cleaned up GitHub workflows (#6)
    * Small update
    
    * Updated name
    
    * Added dependenbot
    
    * Added skip check
    
    * CD into IAC dir
    
    * update terra form init
    
    * Hardcoded values
    
    * update version
    
    * Added version and oidc
    
    * Added client id
    
    * Update extractor
    
    * More cleanup

--- iac/providers.tf
+++ iac/providers.tf
@@ -3,14 +3,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = ">= 3.7.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
